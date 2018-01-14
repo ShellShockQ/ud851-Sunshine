227ec318163f75fea1fb9676978fd930c10256e9
Detrick DeBurr <detrick@digitalflow.com>
2018-01-14 13:19:05 -0600

    --Completed Lesson 2 --Added Fake Weather Data --Added ScrollView

--- S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -17,20 +17,36 @@ package com.example.android.sunshine;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO (1) Create a field to store the weather display TextView
+    TextView mWeatherTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
+        mWeatherTextView = findViewById(R.id.tv_weather_data);
 
-        // TODO (2) Use findViewById to get a reference to the weather display TextView
+        String [] fakeWeatherData ={"" +
+                "Today, Jan 14 - Clear - 32° C / 27° C",
+                "Tomorrow, Jan 15 - Rainy - 35° C / 33° C",
+                "Tue, Jan 16 - Sleet - 62° C / 48° C",
+                "Wed, Jan 17 - Snow - 59° C / 55° C",
+                "Thu, Jan 18 - Windy - 74° C / 64° C",
+                "Fri, Jan 19 - Sunny - 99° C / 97° C",
+                "Sat, Jan 20 - Fog - 67° C / 62° C",
+                "Sun, Jan 21 - Tornado - 95° C / 88° C",
+                "Mon, Jan 22 - Hail - 77° C / 70° C",
+                "Tue, Jan 23 - Cloudy - 72° C / 68° C",
+                "Wed, Jan 24 - Light Rain - 52° C / 47° C",
+                "Wed, Jan 25 - Heavy Rain - 58° C / 50° C"
+        };
 
-        // TODO (3) Create an array of Strings that contain fake weather data
+        for (String forecast : fakeWeatherData){
+            mWeatherTextView.append(forecast + "\n\n\n");
 
-        // TODO (4) Append each String from the fake weather data array to the TextView
+        }
     }
 }
